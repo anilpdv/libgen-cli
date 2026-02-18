5909910fc9c353988b287c2ef131d3e655fe1f15
anilpdv <pdvanil007@gmail.com>
2026-02-18 12:08:43 +0000

    feat: implement root Cobra command and global persistent flags

---
+++ main.go
@@ -0,0 +1,30 @@
+// Copyright © 2019 Antoine Chiny <antoine.chiny@inria.fr>
+// Copyright © 2019 Ryan Ciehanski <ryan@ciehanski.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	libgen_cli "github.com/anilpdv/libgen-cli/cmd/libgen-cli"
+)
+
+func main() {
+	if err := libgen_cli.Execute(); err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+}
